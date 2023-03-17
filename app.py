7060b6061c907c74f1193e94e2ce823d0c9cc4ba
Carlos Rodriguez <carlos.csci@gmail.com>
2023-03-17 16:57:36 -0400

    first push

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Albureame AI", page_icon=":wave:", layout="wide")
+
+#HEADER SECTION
+st.subheader("Hi, I am Chilango :wave:")
+st.title("Una página de Albures")
+st.write("I am passionate to write apps with Python and stuff")
+st.write("[Learn More >](www.google.com)")
