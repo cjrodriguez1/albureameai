619fd0023faf7770c5b93066463a034ec7e3b5d1
Carlos Rodriguez <carlos.csci@gmail.com>
2023-03-17 17:12:06 -0400

    checking txtlist

--- app.py
+++ app.py
@@ -2,16 +2,12 @@ import streamlit as st
 
 st.set_page_config(page_title="Albureame AI", page_icon=":wave:", layout="wide")
 
+txtlist = ["Orale","que ","loco","valedor, te la bañaste"]
+
 #HEADER SECTION
 st.subheader("Hi, I am Chilango :wave:")
 st.title("Una página de Albures")
 st.write("I am passionate to write apps with Python and stuff")
 st.write("[Learn More >](www.google.com)")
 st.write("More stuff ovver here to see if i could deploy continously")
-txt = st.text_area('Text to analyze', '''
-    It was the best of times, it was the worst of times, it was
-    the age of wisdom, it was the age of foolishness, it was
-    the epoch of belief, it was the epoch of incredulity, it
-    was the season of Light, it was the season of Darkness, it
-    was the spring of hope, it was the winter of despair, (...)
-    ''')
+txt = st.text_area('Text to analyze', txtlist)
