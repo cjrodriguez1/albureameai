f7b7b3589ddfe5f6bccd5d12c570ea72ac209bf2
Carlos Rodriguez <carlos.csci@gmail.com>
2023-03-17 17:10:18 -0400

    first attempt at txt area

--- app.py
+++ app.py
@@ -8,3 +8,10 @@ st.title("Una página de Albures")
 st.write("I am passionate to write apps with Python and stuff")
 st.write("[Learn More >](www.google.com)")
 st.write("More stuff ovver here to see if i could deploy continously")
+txt = st.text_area('Text to analyze', '''
+    It was the best of times, it was the worst of times, it was
+    the age of wisdom, it was the age of foolishness, it was
+    the epoch of belief, it was the epoch of incredulity, it
+    was the season of Light, it was the season of Darkness, it
+    was the spring of hope, it was the winter of despair, (...)
+    ''')
