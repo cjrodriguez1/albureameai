dc97b09d06c9d240e9b8af77661cc43c1162055d
Carlos Rodriguez <carlos.csci@gmail.com>
2023-03-21 09:56:21 -0400

    Modified UI looks a bit

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ if 'past' not in st.session_state:
 
 # We will get the user's input by calling the get_text function
 def get_text():
-    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    input_text = st.text_input("","Comencemos", key="input")
     return input_text
 
 user_input = get_text()
